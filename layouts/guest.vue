63d66ccc1db86587c4ab90db70801d63fc5d291a
lukabean24 <lkochlo24@gmail.com>
2023-05-16 17:33:27 +0400

    style: add home page and guest layout

---
+++ layouts/guest.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="w-full min-h-screen h-auto relative">
+    <div class="w-full h-20 bg-whity flex justify-center items-center">
+      <div class="w-5/6 h-full flex justify-start items-center">
+        <div class="w-1/5 h-full flex justify-start items-center">
+          <p class="text-primary text-2xl">EduHub</p>
+        </div>
+        <div class="w-3/5 h-full flex justify-end items-center px-4 space-x-4">
+          <NuxtLink
+            to="/"
+            class="text-primary text-lg hover:text-hover duration-150 cursor-pointer"
+          >
+            Home
+          </NuxtLink>
+          <NuxtLink
+            to="/courses"
+            class="text-primary text-lg hover:text-hover duration-150 cursor-pointer"
+          >
+            Courses
+          </NuxtLink>
+          <NuxtLink
+            to="/about"
+            class="text-primary text-lg hover:text-hover duration-150 cursor-pointer"
+          >
+            About
+          </NuxtLink>
+        </div>
+        <div
+          class="w-1/5 h-full flex justify-start items-center px-4 space-x-4"
+        >
+          <button
+            class="w-40 h-10 rounded-lg bg-primary text-white hover:bg-hover duration-150"
+          >
+            Sign Up
+          </button>
+          <button
+            class="w-40 h-10 rounded-lg bg-transparent border-2 border-primary text-primary hover:border-hover duration-150 hover:text-hover"
+          >
+            Log In
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="w-full h-auto flex justify-center items-center">
+      <div
+        class="w-5/6 h-auto flex flex-col justify-start items-start pt-16 pb-60"
+      >
+        <slot></slot>
+      </div>
+    </div>
+    <div
+      class="absolute w-full h-44 bg-primary bottom-0 left-0 flex justify-center items-center"
+    >
+      <p class="text-3xl text-whity">
+        Big Beautiful Footer From Uncreative Dev
+      </p>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,46 +1,23 @@
 <template>
-  <div class="w-full h-20 bg-whity flex justify-center items-center">
-    <div class="w-5/6 h-full flex justify-start items-center">
-      <div class="w-1/5 h-full flex justify-center items-center">
-        <p class="text-primary text-2xl">EduHub</p>
-      </div>
-      <div class="w-3/5 h-full flex justify-end items-center px-4 space-x-4">
-        <NuxtLink
-          to="/"
-          class="text-primary text-lg hover:text-hover duration-150 cursor-pointer"
-        >
-          Home
-        </NuxtLink>
-        <NuxtLink
-          to="/courses"
-          class="text-primary text-lg hover:text-hover duration-150 cursor-pointer"
-        >
-          Courses
-        </NuxtLink>
-        <NuxtLink
-          to="/about"
-          class="text-primary text-lg hover:text-hover duration-150 cursor-pointer"
-        >
-          About
-        </NuxtLink>
-      </div>
-      <div class="w-1/5 h-full flex justify-start items-center px-4 space-x-4">
-        <button
-          class="w-40 h-10 rounded-lg bg-primary text-white hover:bg-hover duration-150"
-        >
-          Sign Up
-        </button>
-        <button
-          class="w-40 h-10 rounded-lg bg-transparent border-2 border-primary text-primary hover:border-hover duration-150 hover:text-hover"
-        >
-          Log In
-        </button>
-      </div>
+  <div class="w-full h-[550px] flex justify-start items-center">
+    <div class="w-1/2 h-full flex justify-center items-center">
+      <p class="text-dark text-6xl text-center font-extrabold leading-snug">
+        Ignite Your Curiosity, Expand Your Mind.
+      </p>
+    </div>
+    <div class="w-1/2 h-full flex justify-center items-center">
+      <img
+        src="https://img.freepik.com/free-vector/online-certification-illustration_23-2148575636.jpg?w=826&t=st=1684243961~exp=1684244561~hmac=951cab9798bdf3db12be81528ca15945591089b3eb53510dda20450c5d37f554"
+        alt=""
+        class="w-full h-full object-cover object-center"
+      />
     </div>
   </div>
 </template>
 <script setup>
-definePageMeta({});
+definePageMeta({
+  layout: "guest",
+});
 useHead({
   title: "EduHub || Home",
 });
