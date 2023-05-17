2c9b3b6ee57c2e63d213c984dcfc0849f9729243
lukabean24 <lkochlo24@gmail.com>
2023-05-17 17:12:42 +0400

    chore: modify nuxt config file as we moved css file from public folder to assets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxtjs/supabase"],
-  css: ["~/public/css/main.css"],
+  css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
